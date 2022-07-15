799eca0056d3cbce852b28b05b286e595bc0ddd0
Ingrid Aquino <ingridaquinosoares@live.com>
2022-07-15 08:15:55 -0300

    :construction: Projeto de java parte o que estudar

---
+++ src/FrontEnd/FrontEnd.java
@@ -0,0 +1,45 @@
+package FrontEnd;
+
+public class FrontEnd {
+    public  void FrontText() {
+
+        System.out.println(" ---- O que é Front-End ?---- ");
+        System.out.println("");
+
+        System.out.println("Podemos classificar como a parte visual de um site, aquilo que conseguimos interagir.");
+        System.out.println("Quem trabalha com Front End é responsável por desenvolver por meio do código uma interface gráfica\n");
+
+        logica();
+    }
+
+    public  void logica() {
+
+        System.out.println(" --- Lógica de Programação --- ");
+        System.out.println();
+
+        System.out.println("É comum para quem está começando como programador ouvir falar sobre isso, porém poucos dão a devida importância.");
+        System.out.println("Aguçar seu entendimento e raciocínio lógico faz com que você resolva questões não apenas matemáticas, mas assuntos do cotidiano com mais facilidade.\n");
+
+        System.out.println();
+
+        listTech();
+    }
+
+    public  void listTech() {
+
+        System.out.println(" --- Tecnologias para começar --- ");
+        System.out.println();
+
+        System.out.println(" -- Iniciante --");
+        System.out.println("* HTML");
+        System.out.println("* CSS");
+        System.out.println("* JavaScript");
+
+        System.out.println();
+
+        System.out.println(" -- Avançado -- ");
+        System.out.println("* Framework");
+        System.out.println("* Bibliotecas");
+        System.out.println("* API'S");
+    }
+}
---
+++ src/SoftSkills/SoftSkills.java
@@ -0,0 +1,49 @@
+package SoftSkills;
+
+public class SoftSkills {
+
+    public  void SkillText(){
+
+        System.out.println(" --- O que é Soft Skills ? --- ");
+        System.out.println();
+
+        System.out.println("Soft skills são habilidades comportamentais relacionadas a maneira como o profissional ");
+        System.out.println("lida com o outro e consigo mesmo em diferentes situações\n");
+
+        Habilidades();
+    }
+
+    public  void Habilidades(){
+
+        System.out.println(" --- Soft Skills --- ");
+        System.out.println();
+
+        System.out.println("* Princípios éticos");
+        System.out.println("* Confiança");
+        System.out.println("* Atitude positiva");
+        System.out.println("* Motivação");
+        System.out.println("* Trabalho em equipe");
+        System.out.println("* Organização e gestão do tempo");
+        System.out.println("* Capacidade de trabalhar sob pressão");
+        System.out.println("* Comunicação");
+        System.out.println("* Flexibilidade");
+        System.out.println("* Segurança");
+
+        System.out.println();
+        Importancia();
+    }
+
+    public  void Importancia(){
+
+        System.out.println(" --- Qual a importância das Soft Skills ? --- ");
+        System.out.println();
+
+        System.out.println("Existem muitas vantagens estratégicas em contratar pessoas que tenham soft skills desenvolvidas.");
+        System.out.println("Para que a equipe entregue os resultados esperados, um bom relacionamento entre os colegas é essencial.");
+        System.out.println("É aí que entram, por exemplo, as habilidades de comunicação e trabalho em equipe. ");
+        System.out.println("Se os profissionais do time tiverem essas competências, fica fácil estabelecer um dia a dia produtivo e harmonioso.");
+
+        System.out.println();
+
+    }
+}
